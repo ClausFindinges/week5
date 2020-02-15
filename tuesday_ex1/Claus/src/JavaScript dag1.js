b5e2c0a819775c02637b593e5a875e21d488203f
Claus Mikkelsen Findinge <clamifi@hotmail.com>
2020-02-15 14:50:53 +0100

    Add files via upload

---
+++ tuesday_ex1/Claus/src/JavaScript dag1.js
@@ -0,0 +1,31 @@
+
+
+function add(n1, n2){
+
+   return n1 +n2;
+
+}
+
+console.log( add(1,2) );
+
+
+//
+//
+//Function Expression
+//
+//var sub = function(n1,n2){
+//
+//  return n1 - n2
+//
+//} 
+//
+//
+//Callback example
+//
+//var cb = function(n1,n2, callback){
+//
+//  return "Result from the two numbers: "+n1+"+"+n2+"="+callback(n1,n2);
+//
+//};
+
+
